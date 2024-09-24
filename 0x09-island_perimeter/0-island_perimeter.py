d93c13e2b0428b9fb6761efea3daf248489584e3
xola-mthembu <xola.mthembu@gmail.com>
2024-09-24 20:17:20 +0000

    Fix pycodestyle errors for island perimeter function

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """
-This module contains the function that calculates the perimeter of an island
+Module that contains the function to calculate the perimeter of an island.
 """
+
 
 def island_perimeter(grid):
     """
-    Calculates the perimeter of the island in a grid.
-    
+    Calculates the perimeter of the island in the grid.
+
     Args:
         grid (list of list of int): 2D list representing the island.
-    
+
     Returns:
         int: The perimeter of the island.
     """
     perimeter = 0
+
+    # Loop over each row and column in the grid
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                # Start by adding 4 for each land cell
+                # Add 4 for each land cell
                 perimeter += 4
-                # Check if there's land above, subtract 2 if true
-                if i > 0 and grid[i-1][j] == 1:
+
+                # Check if the cell above is land, subtract 2 if true
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                # Check if there's land to the left, subtract 2 if true
-                if j > 0 and grid[i][j-1] == 1:
+
+                # Check if the cell to the left is land, subtract 2 if true
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
+
     return perimeter
